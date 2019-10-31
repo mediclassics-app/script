aa2608c778ace63b5fab41849c92a6ea5720b582
Junho OH <pinedance@gmail.com>
2019-10-31 09:08:49 +0900

    update

--- text/counter.sh
+++ text/counter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tr '[:space:]' '\n' < $1 | sort -S 10G | uniq -c | awk '{print $2"\t"$1}' > $2
+tr '[:space:]' '\n' < $1 | sort -S 10G | uniq -c | sort -nr | awk '{print $2"\t"$1}' > $1".count"
